9d27f07248741d1e4333c43f7af54651fb10447c
AVeemo <yousuf4210@gmail.com>
2026-02-19 19:50:48 +0400

    Add project files.

---
+++ Data.cs
@@ -0,0 +1,56 @@
+﻿using System.Windows.Media;
+
+namespace SolsRNG
+{
+    public class Data
+    {
+        
+        public static KeyValuePair<string, int> CurrentAura = new("None", 1);
+        public static string[] AuraHistory = [];
+
+        public static readonly Dictionary<string, int> Auras = new()
+        {
+            {"Test1", 2},
+            {"Test2", 4},
+            {"Test3", 8},
+            {"Test4", 16},
+            {"Test5", 32},
+            {"Test6", 64},
+            {"Test7", 128},
+            {"Test8", 256},
+            {"Test9", 512},
+            {"Test10", 1024}
+        };
+
+        public static readonly Dictionary<string, SolidColorBrush> Colors = new()
+        {
+            {"Test1", Brushes.Red},
+            {"Test2", Brushes.Orange},
+            {"Test3", Brushes.Yellow},
+            {"Test4", Brushes.LawnGreen},
+            {"Test5", Brushes.CornflowerBlue},
+            {"Test6", Brushes.Indigo},
+            {"Test7", Brushes.BlueViolet},
+            {"Test8", Brushes.Black},
+            {"Test9", Brushes.Gold},
+            {"Test10", Brushes.Silver}
+        };
+        
+        public static KeyValuePair<string, int> GetRandomAura(bool notRare)
+        {
+            KeyValuePair<string, int> chosenAura = new("None", 1);
+            while (chosenAura.Key == "None")
+            {
+                foreach (var aura in Auras)
+                {
+                    if (new Random().Next(0, aura.Value) != 0) continue;
+                    if (aura.Value is 1024 or 512 or 256 or 128 && notRare) break;
+                    chosenAura = aura;
+                    break;
+                }
+            }
+            AuraHistory = [chosenAura.Key, .. AuraHistory];
+            return chosenAura;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
+
+namespace SolsRNG
+{
+
+    public partial class MainWindow : Window
+    {
+        private Storyboard ShuffleAnim;
+        private bool HistoryEnabled  = false;
+        private bool AuraViewEnabled = false;
+        private bool busy            = false;
+
+        private void TogglePanel(object sender, RoutedEventArgs e)
+        {
+            if (sender.ToString().Contains("History"))
+            {
+                if (AuraViewEnabled) return;
+                HistoryEnabled            = !HistoryEnabled;
+                PanelView.Visibility      = !HistoryEnabled ? Visibility.Hidden : Visibility.Visible; // yes i added the not operator here just to make it look good
+                RollButton.IsEnabled      = !HistoryEnabled;
+                AuraViewButton.IsEnabled  = !HistoryEnabled;
+                AuraViewButton.Visibility = !HistoryEnabled ? Visibility.Visible : Visibility.Hidden;
+                PanelView.Items.Clear();
+                if (!HistoryEnabled) return;
+                foreach (var entry in Data.AuraHistory)
+                {
+                    Label newLabel = new()
+                    {
+                        Content = $"{entry} - 1 in {Data.Auras[entry]}",
+                        Foreground = Data.Colors[entry]
+                    };
+                    PanelView.Items.Add(newLabel);
+                }
+            }
+            else
+            {
+                if (HistoryEnabled) return;
+                AuraViewEnabled          = !AuraViewEnabled;
+                PanelView.Visibility     = !AuraViewEnabled ? Visibility.Hidden : Visibility.Visible;
+                RollButton.IsEnabled     = !AuraViewEnabled;
+                HistoryButton.IsEnabled  = !AuraViewEnabled;
+                HistoryButton.Visibility = !AuraViewEnabled ? Visibility.Visible : Visibility.Hidden;
+                PanelView.Items.Clear();
+                if (!AuraViewEnabled) return;
+                foreach (var entry in Data.Auras)
+                {
+                    Label newLabel = new()
+                    {
+                        Content = $"{entry.Key} - 1 in {entry.Value}",
+                        Foreground = Data.Colors[entry.Key]
+                    };
+                    PanelView.Items.Add(newLabel);
+                }
+            }
+        }
+
+        private void RollAura(object sender, RoutedEventArgs e)
+        {
+            if (HistoryEnabled || AuraViewEnabled || busy) return;
+            RollButton.IsEnabled = false;
+            var aura             = Data.GetRandomAura(false);
+            bool trolled         = aura.Value is 1024 or 512 or 256 or 128;
+            AuraText.Content     = aura.Key;
+            AuraText.Foreground  = Data.Colors[aura.Key];
+            Chance.Content       = $"1 in {aura.Value}";
+            Chance.Foreground    = Data.Colors[aura.Key];
+            AuraText.BeginStoryboard(ShuffleAnim);
+            if (trolled)
+            {
+                var fakeOut = Data.GetRandomAura(true);
+                busy        = true;
+                Task.Delay(2250).ContinueWith(_ =>
+                {
+                    Dispatcher.Invoke(() =>
+                    {
+                        AuraText.Content     = fakeOut.Key;
+                        AuraText.Foreground  = Data.Colors[fakeOut.Key];
+                        Chance.Content       = $"1 in {fakeOut.Value}";
+                        Chance.Foreground    = Data.Colors[fakeOut.Key];
+                        RollButton.IsEnabled = true;
+                        busy                 = false;
+                    });
+                });
+            }
+            else
+            {
+                RollButton.IsEnabled = true;
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ShuffleAnim = (Storyboard)rollGrid.Resources["ShuffleAnimation"];
+            KeyDown += ((sender, args) =>
+            {
+                switch (args.Key)
+                {
+                    case Key.H:
+                        TogglePanel("HistoryView", args);
+                        break;
+                    case Key.A:
+                        TogglePanel("AuraView", args);
+                        break;
+                    case Key.R:
+                        RollAura(sender, args);
+                        break;
+                }
+            });
+        }
+    }
+}
